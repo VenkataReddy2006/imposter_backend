b36aa4f0546b0728c36afa48d8dad91e02afca38
VenkataReddy2006 <padalavenkatareddy2006@gmail.com>
2026-07-21 20:47:56 +0530

    Describe your changes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
 });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
